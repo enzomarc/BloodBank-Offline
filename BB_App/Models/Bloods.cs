44f2fa7d42d1f377b16ee95ec3d45a03e3d07be6
Ghost237 <emarc237@gmail.com>
2018-03-22 14:24:32 +0100

    Description delete

--- BB_App/Models/Bloods.cs
+++ BB_App/Models/Bloods.cs
@@ -8,13 +8,6 @@ using MySql.Data.MySqlClient;
 using BB_App.Models;
 using param =  BB_App.Models.Consts;
 
-/* =============================================================
-* Class Name : Blood
-* Description : Performs CRUD on blood over the database.
-* By : Marc Enzo
-* At : 17/03/1018
-* ============================================================= */
-
 namespace BB_App.Models
 {
     /// <summary>
